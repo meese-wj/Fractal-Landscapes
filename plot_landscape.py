7b341614d245ad22fae973e6187f263030dd3535
W. Joe Meese <meese022@umn.edu>
2022-11-06 19:21:30 -0600

    Comment out example plot script

--- plot_landscape.py
+++ plot_landscape.py
@@ -22,16 +22,16 @@ plt.plot(xrange, free_energy_proxy)
 plt.ylim(-1.1, 0)
 
 # Uncomment to make the example in the README.md
-fig, ax = plt.subplots()
-ax.plot(xvals, yvals, lw = 0.5, 
-         marker = "o", mfc = "blue", 
-         ms = 6, mec = "orange", color = "orange")
-# ax.plot( xrange / (1 + epsilon), btlspline(xrange / (1 + epsilon)), lw = 2 )
-ax.set_xticks([])
-ax.set_yticks([])
-ax.set_xlabel("State")
-ax.set_ylabel("Energy")
-ax.set_title("BinaryTree Energy Landscape")
-plt.show()
+# fig, ax = plt.subplots()
+# ax.plot(xvals, yvals, lw = 0.5, 
+#          marker = "o", mfc = "blue", 
+#          ms = 6, mec = "orange", color = "orange")
+# # ax.plot( xrange / (1 + epsilon), btlspline(xrange / (1 + epsilon)), lw = 2 )
+# ax.set_xticks([])
+# ax.set_yticks([])
+# ax.set_xlabel("State")
+# ax.set_ylabel("Energy")
+# ax.set_title("BinaryTree Energy Landscape")
+# plt.show()
 
 
